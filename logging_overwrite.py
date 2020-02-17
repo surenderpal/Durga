020a7996706cc1ea242c0889524f9f02f1b97fa6
surender <surenderpal134@gmail.com>
2020-02-17 12:12:36 +0530

    extra file is deleted

--- logging_overwrite.py
+++
@@ -1,9 +0,0 @@
-import logging
-# logging.basicConfig(filename='logging.txt', level=logging.DEBUG,filemode='w')
-logging.basicConfig()
-print('logging demo')
-logging.critical('This is critical message')
-logging.error('This is error message')
-logging.warning('This is warning message')
-logging.info('This is info message')
-logging.debug('This is debug message')
