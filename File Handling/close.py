8cc0f198d6e765ac48f25506e28b99b3ef57cd3e
surender <surenderpal134@gmail.com>
2020-04-06 22:43:49 +0530

    revising file handling by praticing

--- File Handling/close.py
+++ File Handling/close.py
@@ -6,13 +6,14 @@ f.write('hello this is my third line\n')
 f.write('hello this is my fourth line\n')
 l=['sureder','pal' 'is' 'going' 'to' 'become' 'Data Scientist\n']
 d={'1':'surender','2':'roopa','3\n':'pal\n'}
-f.writelines(d)
+# f.writelines(d)
 f.writelines(l)
-print('file Name:',f.name)
-print('file mode:',f.mode)
-print('Wether file is closed:',f.closed)
-print('Weather file is readable:',f.readable())
-print('Weather file is writable:',f.writable())
-
+# print('file Name:',f.name)
+# print('file mode:',f.mode)
+# print('Wether file is closed:',f.closed)
+# print('Weather file is readable:',f.readable())
+# print('Weather file is writable:',f.writable())
+# f.writelines(d['2'])
+f.writelines(d.values())
 f.close()
 
