8c58a34b9c57c85b47a92bba169c8ccc2edd53a1
surender <surenderpal134@gmail.com>
2020-04-08 23:29:22 +0530

    changes in lines

--- File Handling/No_of_lines.py
+++ File Handling/No_of_lines.py
@@ -3,14 +3,16 @@ fname=input('Enter file name:')
 if os.path.isfile(fname):
     print('File exists!!!',fname)
     with open(fname,'r') as f:
-        lcount=wcount=0
+        lcount=wcount=ccount=0
         for line in f:
             if f.readline!='':
                 lcount=lcount+1
             no_of_words_in_current_line=len(line.split())
             wcount=wcount+no_of_words_in_current_line
-
+            no_of_char_in_current_line=len(line)
+            ccount=ccount+no_of_char_in_current_line
         print('No of lines in file is:',lcount)
         print('No of words in file are:',wcount)
+        print('No of characters in file are:',ccount)
 else:
     print("file doesn't exists",fname)
