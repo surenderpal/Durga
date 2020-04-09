e19622f508c48ce86949d57e9a231795aabb0809
surender <surenderpal134@gmail.com>
2020-04-09 18:20:56 +0530

    reading csv file

--- File Handling/reading_csv.py
+++ File Handling/reading_csv.py
@@ -1,4 +1,11 @@
 import csv
 with open('emp.csv','r') as f:
-    w=csv.reader(f)
-    
+    r=csv.reader(f)
+    data=list(r)
+    # print(data)
+    for row in data:
+        # print(row)
+        for column in row:
+            print(column,'\t',end='')
+        print()
+
