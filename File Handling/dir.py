0aa05e834d6bcf5bc6b441272979659c8e413727
surender <surenderpal134@gmail.com>
2020-04-09 22:07:30 +0530

    working with directory-mkdir,getcwd

---
+++ File Handling/dir.py
@@ -0,0 +1,8 @@
+import os
+cwd=os.getcwd()
+# print("Current working directory:",cwd)
+# os.mkdir('Valmiki')
+# print('dir Valmike created')
+os.mkdir('Valmiki12/varun1')
+print('Directory created')
+ 
