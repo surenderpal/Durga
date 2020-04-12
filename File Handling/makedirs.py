08bf8b458ecb64e2720f3592262cc854cf3d873d
surender <surenderpal134@gmail.com>
2020-04-12 23:20:36 +0530

    listdir and walk function

--- File Handling/makedirs.py
+++ File Handling/makedirs.py
@@ -1,5 +1,15 @@
 import os
 # os.makedirs('Valmiki/varun/sandesh')
 # os.makedirs('Videos/pythonVideos/filehandling')
-os.rmdir('Videos/pythonVideos')
+# os.rmdir('Videos/pythonVideos')
 # os.removedirs('Videos/pythonVideos/filehandling')
+# os.rename('Videos/pythonVideos','Videos/Python Videos')
+# os.makedirs('cwd/material/java/selenium') 
+# os.rmdir('cwd/material/java/java1.txt')
+# os.makedirs('cwd/material/java/selenium')
+# os.rename('cwd/material/testing/selenium/java3.txt','cwd/material/testing/selenium/testing3.txt')
+# l=os.listdir('cwd/material')
+l=os.walk('cwd')
+# print(type(l))
+for g in l:
+    print(g)
