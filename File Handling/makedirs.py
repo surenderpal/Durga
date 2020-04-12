e666b4d2037ab10d1743b35f2c5a2803d18de9f1
surender <surenderpal134@gmail.com>
2020-04-12 22:50:17 +0530

    rmdir/removedirs

--- File Handling/makedirs.py
+++ File Handling/makedirs.py
@@ -1,3 +1,5 @@
 import os
 # os.makedirs('Valmiki/varun/sandesh')
-os.makedirs('Videos/pythonVideos/filehandling')
+# os.makedirs('Videos/pythonVideos/filehandling')
+os.rmdir('Videos/pythonVideos')
+# os.removedirs('Videos/pythonVideos/filehandling')
