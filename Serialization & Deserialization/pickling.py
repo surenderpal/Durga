534abbe0258f4cfcc6424ced737b739ed7883692
surender <surenderpal134@gmail.com>
2020-04-13 18:01:45 +0530

    praticed serializatio and deserialization

--- Serialization & Deserialization/pickling.py
+++ Serialization & Deserialization/pickling.py
@@ -9,10 +9,9 @@ class employee:
         print("Emp NO: {}, Emp Name: {}, Emp Salary:{}, Emp Adderess:{}".format(self.eno,self.ename,self.esal,self.eaddr))
 
 e=employee(100,'surender',10000,'Delhi')
-e=employee(101,'rahul',1000,'Dwarka')
-with open('emp.ser','wb') as f:
+e.display()
+with open('emp.surender','wb') as f:
     pickle.dump(e,f)
-
-with open('emp.ser','rb') as f:
-    new_obj=pickle.load(f)
-new_obj.display()
+with open('emp.surender','rb') as f:
+    obj=pickle.load(f)
+    obj.display()
