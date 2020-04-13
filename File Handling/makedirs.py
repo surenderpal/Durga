eee266d1d054d7333eec95f7702128fb85373f99
surender <surenderpal134@gmail.com>
2020-04-13 12:39:33 +0530

    return type of listdir,<class ' built-in_function_or_method'>

--- File Handling/makedirs.py
+++ File Handling/makedirs.py
@@ -8,13 +8,14 @@ import os
 # os.rmdir('cwd/material/java/java1.txt')
 # os.makedirs('cwd/material/java/selenium')
 # os.rename('cwd/material/testing/selenium/java3.txt','cwd/material/testing/selenium/testing3.txt')
-# l=os.listdir('cwd/material')
-l=os.walk('cwd')
+l=os.listdir('cwd/material')
+print(type(l))
+# l=os.walk('cwd')
 # print(type(l))
 # for g in l:
 #     print(g)
-for path,name,file1 in l:
-    print('Dir path:',path)
-    print('Dir name:',name)
-    print('Dir file:',file1)
-    print()
+# for path,name,file1 in l:
+#     print('Dir path:',path)
+#     print('Dir name:',name)
+#     print('Dir file:',file1)
+#     print()
