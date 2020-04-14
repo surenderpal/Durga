7b7767f9ce45163b88bb85aa981d316d2e9b41cd
surender <surenderpal134@gmail.com>
2020-04-14 18:53:44 +0530

    Deserialization from Json string

--- Serialization & Deserialization/json-deserialization.py
+++ Serialization & Deserialization/json-deserialization.py
@@ -9,10 +9,11 @@ json_strint='''{
 display=json.loads(json_strint)
 print(type(display))
 print(display)
-{
-    'name': 'surender', 
-    'age': 30, 
-    'salary': 1000, 
-    'isMarried': True, 
-    'IsHavingGF': None
-}
+print('Emp Name:',display['name'])
+print('Emp Age:',display['age'])
+print('Salary',display['salary'])
+print('Is Emp Married:',display['isMarried'])
+print('Is Emp Haing GF:',display['IsHavingGF'])
+
+for k,v in display.items():
+    print(k,':',v)
