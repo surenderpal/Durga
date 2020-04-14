bc954798082707555822eb919994dcb72795d678
surender <surenderpal134@gmail.com>
2020-04-14 17:40:20 +0530

    added indent=4 and sort_keys=True

--- Serialization & Deserialization/jason.py
+++ Serialization & Deserialization/jason.py
@@ -1,4 +1,5 @@
 import json
 employee={'namee':'surender','age':35,'salary':1000.0,'ismarried':True,'isHavingGF':None}
-json_string=json.dumps(employee)
+json_string=json.dumps(employee,indent=1,sort_keys=True) #serialize to string
 print(json_string)
+# {"namee": "surender", "age": 35, "salary": 1000.0, "ismarried": true, "isHavingGF": null}
