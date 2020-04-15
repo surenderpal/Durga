43bf74e00a65680ec4c289f0efd7486863bce80a
surender <surenderpal134@gmail.com>
2020-04-15 23:36:37 +0530

    converted custom class to dict

--- Serialization & Deserialization/custom_class.py
+++ Serialization & Deserialization/custom_class.py
@@ -6,6 +6,7 @@ class Employee:
         self.esal=esal
         self.eaddr=eaddr
     def display(self):
-        print('Employee No is {}, Employee Name is {}, Employee Salarys is {} and Employee Address is{}'.format(self.eno,self.ename,self.esal,self.eaddr))
+        print('Employee No is {}, \nEmployee Name is {}, \nEmployee Salarys is {} and \nEmployee Address is {}'.format(self.eno,self.ename,self.esal,self.eaddr))
 e=Employee(100,'surender',1000,'Delhi')
-e.display()
+e_dict={'eno':e.eno,'ename':e.ename,'esal':e.esal,'eaddr':e.eaddr}
+print('converted custom class to Dict:',e_dict)
