6e15ba001127486f8b6e66eb37f854b164fb3ba5
surender <surenderpal134@gmail.com>
2020-04-16 19:39:50 +0530

    deserializing JSON string/ file to emp object

--- Serialization & Deserialization/json_pickle.py
+++ Serialization & Deserialization/json_pickle.py
@@ -12,7 +12,16 @@ e=Employee(100,'surender',1000.0,'Delhi',False)
 # e.display()
 #serailzing to json string
 json_str=jsonpickle.encode(e)
-print('encode',json_str)
+# print('encode',json_str)
 #serializationto JSON File
-with open('cust_emp.json','w') as f:
-    f.write(json_str)
+# with open('cust_emp.json','w') as f:
+#     f.write(json_str)
+#Deserialization from json string
+e2=jsonpickle.decode(json_str)
+# print(type(e2))
+# e2.display()
+#Deserialization from json file
+with open('cust_emp.json','r') as f:
+    j_string=f.readline()
+e3=jsonpickle.decode(j_string)
+e3.display()
