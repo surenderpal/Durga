b8f0bdf4bc3d55e0ac26fe420072a54907927a11
surender <surenderpal134@gmail.com>
2020-04-16 19:26:30 +0530

    serialization to json string/json file using encode

--- Serialization & Deserialization/json_pickle.py
+++ Serialization & Deserialization/json_pickle.py
@@ -1,3 +1,4 @@
+import jsonpickle
 class Employee:
     def __init__(self,eno,ename,esal,eaddr,ismarried):
         self.eno=eno
@@ -7,5 +8,11 @@ class Employee:
         self.ismarried=ismarried
     def display(self):
         print('Employee no is {},\nEmployee name is {},\nEmployee Salary is {}, \nEmployee Address is {},\nIs Employee Married {}'.format(self.eno,self.ename,self.esal,self.eaddr,self.ismarried))
-e=Employee(100,'surender',1000.0,'Delhi','False')
-e.display()
+e=Employee(100,'surender',1000.0,'Delhi',False)
+# e.display()
+#serailzing to json string
+json_str=jsonpickle.encode(e)
+print('encode',json_str)
+#serializationto JSON File
+with open('cust_emp.json','w') as f:
+    f.write(json_str)
