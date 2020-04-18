11624fa6918a379bebd6ca7243e2ff5801f71b05
surender <surenderpal134@gmail.com>
2020-04-18 23:25:36 +0530

    print line changed

--- Decorator/decor_div.py
+++ Decorator/decor_div.py
@@ -2,7 +2,7 @@ def decor_for_devision(func):
     def inner(a,b):
         if b ==0:
             print('#'*50)
-            print("Can't devide with 0, try again")
+            print("Hello Stupid, How we can divide with Zero, you idiot !!!")
             print('*'*50)            
         else:
             func(a,b)
