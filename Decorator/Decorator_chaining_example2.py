b98158f83af28f7b32ef69550658c541f87d49a9
surender <surenderpal134@gmail.com>
2020-04-19 21:37:39 +0530

    Decorator Position changed

--- Decorator/Decorator_chaining_example2.py
+++ Decorator/Decorator_chaining_example2.py
@@ -3,7 +3,14 @@ def decor1(func):
         x=func()
         return x*x
     return inner1
+def decor2(func):
+    def inner2():
+        x=func()
+        return 2*x
+    return inner2
+@decor1
 @decor1
 def num():
     return 20
 print(num())
+
