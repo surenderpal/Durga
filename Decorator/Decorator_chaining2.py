00eca70dd8b835b5fe2d139d061a4c845b1d6052
surender <surenderpal134@gmail.com>
2020-04-19 20:54:46 +0530

    Calling inner funciton in Decor1 and 2

---
+++ Decorator/Decorator_chaining2.py
@@ -0,0 +1,15 @@
+def decor_for_f(func):
+    def inner1():
+        print('Decor 1 execution')
+        func()
+    return inner1
+def decor2_for_f(func):
+    def inner2():
+        print('Decor 2 execution')
+        func()
+    return inner2
+@decor2_for_f    
+@decor_for_f
+def f():
+    print('Original function')
+f()
