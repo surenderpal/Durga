1529094eeb955dbb39d946a83e16577a0ff338fc
surender <surenderpal134@gmail.com>
2020-04-20 16:04:58 +0530

    first N fibonacci number, using input function

--- Generators/fibonacci_generator.py
+++ Generators/fibonacci_generator.py
@@ -5,8 +5,15 @@ def fibonacci_generator():
         yield a
         a,b= b,a+b
 g=fibonacci_generator()    
-for x in g:
-    if x<=100:
-        print(x)
-    else:
-        break
+# for x in g:
+#     if x<=100:
+#         print(x)
+#     else:
+#         break
+
+#first N fibonacci numbers
+count=1
+n=int(input('Enter number of fibonacci numbers Required:'))
+while count<=n:
+    print(next(g))
+    count=count+1
