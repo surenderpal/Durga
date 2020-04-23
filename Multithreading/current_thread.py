c6ed7ba3aee4dbc12565b4cb08292f53121b14db
surender <surenderpal134@gmail.com>
2020-04-23 17:39:15 +0530

    Running Child and Main thread

--- Multithreading/current_thread.py
+++ Multithreading/current_thread.py
@@ -1,10 +1,8 @@
 from threading import *
 def display():
-    print('This code executed by child Thread:',current_thread().getName())
-# display()
-t=Thread(target=display) #MainThread creates child thread object
-t.start() #Main thread starts ChildThread      
-u=Thread(target=display)
-u.start()
-print('This code executed by Thread:',current_thread().getName())
-   
+    for i in range(10):
+        print('Child Thread')
+t=Thread(target=display)
+t.start()
+for i in range(10):
+    print('Main Thread')
