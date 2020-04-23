3dbea6389434f3eb6452e56c650f5064c0cf3ed1
surender <surenderpal134@gmail.com>
2020-04-23 19:44:51 +0530

    Adding 4 thread

--- Multithreading/without_extending_thread_class.py
+++ Multithreading/without_extending_thread_class.py
@@ -2,10 +2,16 @@ from threading import *
 class test:
     def display(self):
         for i in range(10):
-            print('Child Thread-2')
+            print('Child Thread executing Name:',current_thread().getName())
 o=test()
-# t=Thread(target=test().display)
-t=Thread(target=o.display)
-t.start()
-for i in range(10):
-    print('Main Thread')
+# t=Thread(target=test().display) 
+t1=Thread(target=o.display)
+t2=Thread(target=o.display)
+t3=Thread(target=o.display)
+t4=Thread(target=o.display)
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+# for i in range(10):
+#     print('Main Thread Executing Name: ',current_thread().getName())
