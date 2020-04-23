5cb229969ebef6e74c52f7888d4c79ba9e240126
surender <surenderpal134@gmail.com>
2020-04-23 19:27:59 +0530

    creating a thread by extending thread class

---
+++ Multithreading/extending_thread_class.py
@@ -0,0 +1,10 @@
+from threading import *
+# class MyThread(Thread):
+#     def run(self):
+#         for i in range(10):
+#             print('Child Thread-1')
+# t=MyThread()
+# t.start()
+# for i in range(10):
+#     print('Main method-1')
+
