9fdf5de2b2dd29ae74c773a6d6ce049d14029d49
surender <surenderpal134@gmail.com>
2020-04-25 23:53:37 +0530

    setting child thread as Daemon Thread

--- Multithreading/Daemon_thread_terminates.py
+++ Multithreading/Daemon_thread_terminates.py
@@ -2,10 +2,10 @@ from threading import *
 import time
 def display():
     for i in range(10):
-        print('Lazy Daemon')
+        print('Lazy Daemon',i)
         time.sleep(2)
-t=Thread(target=display)
-t.setDaemon(True)
-t.start()
-time.sleep(5)
-print('End of main thread')
+t=Thread(target=display) # t is non-daemon by nature
+t.setDaemon(True) #we set the t explicitly as Daemon
+t.start() 
+time.sleep(5) #we stopped the non-daemon thread
+print('End of main thread') #Daemon thread automatically stopped
