bf7821773d44dccb532d591f1606603ac01e596f
surender <surenderpal134@gmail.com>
2020-04-28 13:16:53 +0530

    semaphore behaviour: releas can exceeds to the no. of acquire call

---
+++ Multithreading/Normal_semaphore.py
@@ -0,0 +1,9 @@
+from threading import *
+# s=Semaphore(2)
+s=BoundedSemaphore(2)
+s .acquire()
+print('Main thread trying to acquire lock...')
+s.acquire()
+print('Main thread trying to acquire lock second time....')
+s.acquire()
+print('Main thread trying to acquire lock third time...')
