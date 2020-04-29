aa8b70095497a4b9f73b214485134c0a9ecd2641
surender <surenderpal134@gmail.com>
2020-04-29 16:41:19 +0530

    Added Release code

--- Multithreading/Normal_semaphore.py
+++ Multithreading/Normal_semaphore.py
@@ -1,10 +1,14 @@
 # In semaphore no of release call can exceeds the acquire call
 from threading import *
-# s=Semaphore(2)
-s=BoundedSemaphore(2)
+s=Semaphore(3)
+# s=BoundedSemaphore(2)
 s .acquire()
 print('Main thread trying to acquire lock...')
 s.acquire()
 print('Main thread trying to acquire lock second time....')
 s.acquire()
 print('Main thread trying to acquire lock third time...')
+s.release()
+s.release()
+s.release()
+s.release()
