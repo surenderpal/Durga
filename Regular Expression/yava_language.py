55a0fefc343e5b3cc22f010b5d13c672815e9623
surender <surenderpal134@gmail.com>
2020-05-06 22:47:42 +0530

    validating the REGEX

---
+++ Regular Expression/yava_language.py
@@ -0,0 +1,14 @@
+# 1.The allowed characters are:
+#   alphabets
+#   digits
+#   #
+
+#2. First character should be lower case alphabet symbol from a to k
+
+#3. The second character should be any digit divisible by 3
+# [0369] [3690]
+#4. The length of identifier should be atleast 2
+
+# [a-k][3690][a-zA-Z0-9#]*  
+import re
+s=input('Enter identifier to validate:')
