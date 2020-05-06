6ab3c69b18e44a9fd9f1dedd2760d585c42c1126
surender <surenderpal134@gmail.com>
2020-05-06 23:03:37 +0530

    yava language identifier REGEX

--- Regular Expression/yava_language.py
+++ Regular Expression/yava_language.py
@@ -3,12 +3,17 @@
 #   digits
 #   #
 
-#2. First character should be lower case alphabet symbol from a to k
+# 2. First character should be lower case alphabet symbol from a to k
 
-#3. The second character should be any digit divisible by 3
-# [0369] [3690]
-#4. The length of identifier should be atleast 2
+# 3. The second character should be any digit divisible by 3
+#    [0369] [3690]
+# 4. The length of identifier should be atleast 2
 
 # [a-k][3690][a-zA-Z0-9#]*  
 import re
 s=input('Enter identifier to validate:')
+m=re.fullmatch('[a-k][3690][a-zA-Z0-9#]*',s)
+if m!=None:
+    print(s,'is valid yava Identifier')
+else:
+    print(s,'is Invalid yava Identifier')
